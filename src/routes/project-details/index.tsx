611456b653d7b97fdf54c45229f07b04a66732ce
Aaron <aaronbartholomew2005@outlook.com>
2023-07-22 10:52:12 +0200

    forms

--- src/routes/project-details/index.tsx
+++ src/routes/project-details/index.tsx
@@ -13,17 +13,12 @@ export default component$(() => {
 
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="projectName" class="form-label">Project name</label>
+        <input class="form-control" id="projectName" />
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <input class="form-control" id="exampleInputPassword1" />
       </div>
 
       <AreaOfInterest />
