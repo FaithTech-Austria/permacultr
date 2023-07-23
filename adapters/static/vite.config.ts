d7a2b451256a4597d886c09de818e4a1857f066a
Daniel Beeke <mail@danielbeeke.nl>
2023-07-23 17:19:20 +0200

    does it work now?

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -4,6 +4,7 @@ import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
   return {
+    base: '/permacultur',
     build: {
       ssr: true,
       rollupOptions: {
