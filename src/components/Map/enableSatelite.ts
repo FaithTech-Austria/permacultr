4d767c85b84000c63c8ba1470ef3441aeef5eb71
Daniel Beeke <mail@danielbeeke.nl>
2023-08-04 17:28:14 +0200

    adding oogle satelite

--- src/components/Map/enableSatelite.ts
+++ src/components/Map/enableSatelite.ts
@@ -7,8 +7,10 @@ export const enableSatelite = $((map: Map) => {
   map.addSource('satelite', {
     'type': 'raster',
     'tiles': [
-        `https://api.maptiler.com/maps/satellite/{z}/{x}/{y}@2x.jpg?key=${mapTilerKey}`
+        // `https://api.maptiler.com/maps/satellite/{z}/{x}/{y}@2x.jpg?key=${mapTilerKey}`
+        'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
     ],
+    maxzoom: 21,
     'tileSize': 256
   });
   map.addLayer(
